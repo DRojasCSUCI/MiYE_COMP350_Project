eb54cbac1929b9bfe4b18369e9b223763e2c71e8
DRojasCSUCI <daniel.rojasbotello918@myci.csuci.edu>
2021-10-16 11:30:39 -0700

    Optimized performance when re-building an image and fixed bug where github repository would be indefinetly cached

--- container/run_application_in_container.bat
+++ container/run_application_in_container.bat
@@ -6,9 +6,10 @@
 :: Run Terminal FIle in Order to Run Application Branch Specified in Dockerfile through Container
 ::
 
+:: %DATE%%TIME% Are Windows Pseudo-Varibles(Dynamic variables), meaning it won't work on mac or linux
+
 :: docker build --no-cache -t miye:image .
-docker build --build-arg NOCACHE=$(date +%s) -t miye:image
-:: docker build -t miye:image .
+docker build -t miye:image --build-arg CACHEBUST="%DATE%%TIME%" . 
 docker run --name miye_comp350_container -it miye:image
 docker stop miye_comp350_container
 docker rm miye_comp350_container
