51f4e0d34df88803befcd7bbb0a2e253435ddf4d
Jeffery Foyil <jefferyfoyil@gmail.com>
2022-05-20 09:34:26 -0700

    First commit

--- src/main/java/org/vaadin/code/ServiceManager.java
+++ src/main/java/org/vaadin/code/ServiceManager.java
@@ -73,4 +73,93 @@ public class ServiceManager {
 
     }
 
+    public void addService(Connection conn, String serviceID, String serviceName, String serviceDescription, float pricePerMinute, String duration) throws SQLException{
+
+        // Preparing the query
+        String sql =
+                "INSERT INTO SERVICES " +
+                        "(SERVICE_ID, SERVICE_NAME, SERVICE_DESC, PRICE_PER_MINUTE, DURATION_OPTIONS)" +
+                        "VALUES (?, ?, ?, ?, ?)";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        pstmt.setString(1, serviceID);
+        pstmt.setString(2, serviceName);
+        pstmt.setString(3, serviceDescription);
+        pstmt.setFloat(4, pricePerMinute);
+        pstmt.setString(5, duration);
+        pstmt.executeUpdate();
+        System.out.println("Added new service.");
+    }
+
+    public void removeService(Connection conn, String serviceID) throws SQLException {
+        // Preparing the query
+        String sql = "DELETE FROM SERVICES WHERE SERVICE_ID = \"" + serviceID + "\"";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        pstmt.executeUpdate();
+        System.out.println("Deleted Service.");
+    }
+
+    public void modifyService(Connection conn, String serviceID) throws SQLException{
+        // check if service ID is in table
+        System.out.println("What would you like to modify?\n" +
+                "[1] Name\n" +
+                "[2] Description\n" +
+                "[3] Price per Minute\n" +
+                "[4] Duration Options\n");
+        Scanner scan = new Scanner(System.in);
+
+        int userIn = scan.nextInt();
+        scan.nextLine();
+
+        String sql;
+        PreparedStatement pstmt;
+
+        switch (userIn){
+            case 1:
+                // edit the name
+                System.out.print("Please input the new name: ");
+                String name = scan.nextLine();
+                // Preparing the query
+                sql = "UPDATE SERVICES SET SERVICE_NAME = \"" + name + "\" WHERE SERVICE_ID = \"" + serviceID + "\"";
+                pstmt = conn.prepareStatement(sql);
+                pstmt.executeUpdate();
+                System.out.println("Name Changed.");
+                break;
+            case 2:
+                // edit the description
+                System.out.print("Please input the new description: ");
+                String description = scan.nextLine();
+                // Preparing the query
+                sql = "UPDATE SERVICES SET SERVICE_DESC = \"" + description + "\" WHERE SERVICE_ID = \"" + serviceID + "\"";
+                pstmt = conn.prepareStatement(sql);
+                pstmt.executeUpdate();
+                System.out.println("Description Changed.");
+                break;
+            case 3:
+                // edit the price
+                System.out.print("Please input the new price per minute: ");
+                float price = scan.nextFloat();
+                scan.nextLine();
+                // Preparing the query
+                sql = "UPDATE SERVICES SET PRICE_PER_MINUTE = " + price + " WHERE SERVICE_ID = \"" + serviceID + "\"";
+                pstmt = conn.prepareStatement(sql);
+                pstmt.executeUpdate();
+                System.out.println("Price Changed.");
+                break;
+            case 4:
+                // edit the duration
+                System.out.print("Please input the new duration options: ");
+                String duration = scan.nextLine();
+                // Preparing the query
+                sql = "UPDATE SERVICES SET PRICE_PER_MINUTE = \"" + duration + "\" WHERE SERVICE_ID = \"" + serviceID + "\"";
+                pstmt = conn.prepareStatement(sql);
+                pstmt.executeUpdate();
+                System.out.println("Duration Changed.");
+                break;
+            default:
+                System.out.println("Invalid Input");
+        }
+    }
+
 }
